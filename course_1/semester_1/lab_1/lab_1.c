57dfa1509fce53d7274c96bbc8ea7d388d067ffc
Artem <artempetrashkevich8@gmail.com>
2026-09-15 14:28:55 +0300

    Add the program's core structure

---
+++ course_1/semester_1/lab_1/lab_1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+
+
+    return 0;
+}
+
+
+
+int get_mode() {
+    return 0;
+}
+
+
+int formula() {
+    return 0; 
+}
+
+
+
+
+int cycle(): {
+    ...
+}
